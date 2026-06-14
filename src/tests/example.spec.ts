d57174f442fbb11ed08de4e5a12506575a9ab392
Matthew Radulovich <daemon6109@gmail.com>
2026-06-14 03:03:17 +0000

    added testing setup

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,3 +1,15 @@
-import { describe } from "@rbxts/jest-globals";
+import { describe, expect, it } from "@rbxts/jest-globals";
 
-describe;
+describe("Smoke Test", () => {
+	it("should pass", () => {
+		expect(true).toBe(true);
+	});
+
+	it("should add numbers", () => {
+		expect(2 + 2).toBe(4);
+	});
+
+	it("should compare strings", () => {
+		expect("Anime Reborn II").toBe("Anime Reborn II");
+	});
+});
